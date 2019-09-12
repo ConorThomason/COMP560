63b78149c978e8c1d2dca1c5f8070968abbb9023
Conor Thomason <conorth@ad.unc.edu>
2019-09-12 19:02:34 -0400

    Successful importing of mapping constraints; now for the array-specific requirements

--- ConorThomasonHW1KenKen/src/com/conorthomason/model/DataFileImporter.java
+++ ConorThomasonHW1KenKen/src/com/conorthomason/model/DataFileImporter.java
@@ -2,7 +2,6 @@ package com.conorthomason.model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -16,13 +15,15 @@ public class DataFileImporter {
     }
 
     public void importData() throws FileNotFoundException {
-        Scanner input = new Scanner(new File("Data.txt"));
+        Scanner input = new Scanner(new File("src/Data"));
         int arraySize = Integer.parseInt(input.nextLine());
         dataArray = new int[arraySize][arraySize];
+        constrainedArray = new char[arraySize][arraySize];
         try {
             for (int i = 0; i < arraySize; i++) {
+                String currentLine = input.next();
                 for (int j = 0; j < arraySize; j++) {
-                    constrainedArray[i][j] = input.next().charAt(j);
+                    constrainedArray[i][j] = currentLine.charAt(j);
                 }
             }
         } catch (NullPointerException e){
@@ -49,8 +50,21 @@ public class DataFileImporter {
         return true;
     }
 
-    public void printConstrainedArray(){
-        System.out.println(Arrays.deepToString(constrainedArray));
+    public void printConstrainedArray() {
+        for (int i = 0; i < constrainedArray.length; i++) {
+            for (int j = 0; j < constrainedArray.length; j++) {
+                System.out.print(constrainedArray[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public void printDataArray() {
+        for (int i = 0; i < dataArray.length; i++) {
+            for (int j = 0; j < dataArray.length; j++) {
+                System.out.print(dataArray[i][j]);
+            }
+            System.out.println();
+        }
     }
 
 }
