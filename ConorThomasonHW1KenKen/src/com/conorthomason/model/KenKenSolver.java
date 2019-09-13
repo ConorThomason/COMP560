6ed62fc5db66f61e6eff2977615aab9dfb52e1a2
Conor Thomason <conorth@ad.unc.edu>
2019-09-13 15:39:07 -0400

    Trying to solve index issues

--- ConorThomasonHW1KenKen/src/com/conorthomason/model/KenKenSolver.java
+++ ConorThomasonHW1KenKen/src/com/conorthomason/model/KenKenSolver.java
@@ -6,8 +6,6 @@ public class KenKenSolver {
     private char constrainedArray[][];
     private int dataArray[][];
     private int arraySize;
-    private int r = 0; //Current row
-    private int c = 0; //Current column
     private TreeMap<String, SolutionConstraint> constraints;
 
     public KenKenSolver(char constrainedArray[][], int dataArray[][], TreeMap constraints) {
@@ -19,35 +17,41 @@ public class KenKenSolver {
 
     public int[][] solveKenKen() {
 
-        if (simpleBacktrackSolve())
+        if (simpleBacktrackSolve(0, 0))
             return dataArray;
         else
             return dataArray; //If it returns false, that means no solution was found.
     }
 
-    public int findValid() {
+    public int findValid(int row, int column) {
         for (int i = 0; i < arraySize; i++) {
-            if (validityCheck(i))
-                return i;
+            if (validityCheck(i + 1, row, column))
+                return i + 1;
         }
         return 0;
     }
 
-    public boolean simpleBacktrackSolve() {
-        if (filledArray())
+    public boolean simpleBacktrackSolve(int row, int column) {
+        if (filledArray(row, column))
             return true;
         else {
-            int validValue = findValid();
+            int validValue = findValid(row, column);
             if (validValue != 0) {
-                dataArray[r][c] = validValue;
+                dataArray[row][column] = validValue;
+                if (row != arraySize)
+                    return simpleBacktrackSolve(++row, column);
+                else
+                    return simpleBacktrackSolve(row, ++column);
+            }
+            else {
+                return false;
             }
         }
-        return false;
     }
-        public boolean filledArray() {
+        public boolean filledArray(int r, int c) {
             return (r == arraySize && c == arraySize) ? true : false;
         }
-        public boolean validityCheck(int v){
+        public boolean validityCheck(int v, int r, int c){
             //Checks if duplicate number is in row or column
             for (int i = 0; i < dataArray.length; i++) {
                 if (v == dataArray[r][i] || v == dataArray[i][c]) {
