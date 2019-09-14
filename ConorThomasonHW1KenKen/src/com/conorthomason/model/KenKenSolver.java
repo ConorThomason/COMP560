b0ec394114db01d94a4992ff1ce8fe1a5ceeae58
Conor Thomason <conorth@ad.unc.edu>
2019-09-14 17:20:29 -0400

    Successfully filled the data array with a solution, now to add additional constraints to account for box

--- ConorThomasonHW1KenKen/src/com/conorthomason/model/KenKenSolver.java
+++ ConorThomasonHW1KenKen/src/com/conorthomason/model/KenKenSolver.java
@@ -17,66 +17,53 @@ public class KenKenSolver {
 
     public int[][] solveKenKen() {
 
-        if (simpleBacktrackSolve(0, 0))
+        if (simpleBacktrackSolve())
             return dataArray;
         else
             return dataArray; //If it returns false, that means no solution was found.
     }
 
-    public boolean simpleBacktrackSolve(int row, int column){
-        if (filledArray()){
+    public boolean simpleBacktrackSolve(){
+        int row = -1;
+        int column = -1;
+        boolean isEmpty = true;
+        for (int i = 0; i < arraySize; i++){
+            for (int j = 0; j < arraySize; j++){
+                if (dataArray[i][j] == 0){
+                    row = i;
+                    column = j;
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if (!isEmpty){
+                break;
+            }
+        }
+        if (isEmpty){
             return true;
         }
-        else{
-            int possibleIterations = 6;
-            while (possibleIterations <= 6){
-
-                //Limiting the possible iterations, for repeated generated values
-
-                //Generate a value, takes into account whatever is in the current index
-                int generatedValue = generatedValidValue(dataArray[row][column], row, column);
-
-                //If it's -1, there are no valid values.
-                if (generatedValue != -1) {
-                    possibleIterations++;
-                    dataArray[row][column] = generatedValue;
-                    boolean result;
-                    if (column == arraySize - 1)
-                        result = simpleBacktrackSolve(++row, 0);
-                    else
-                        result = simpleBacktrackSolve(row, ++column);
-                    if (result)
-                        continue;
-                    else
-                        return true;
-                }
-                //Returning false SHOULD reset the previous value, and continue attempts. Will work up until it works.
+        for (int attempt = 1; attempt <= arraySize; attempt++){
+            if (safeValueCheck(attempt, row, column)){
+                dataArray[row][column] = attempt;
+                if (simpleBacktrackSolve())
+                    return true;
                 else
-                    return false;
+                    dataArray[row][column] = 0;
             }
         }
         return false;
     }
 
-    public boolean filledArray(){
+    public boolean safeValueCheck(int value, int row, int column){
         for (int i = 0; i < arraySize; i++){
-            for (int j = 0; j < arraySize; j++){
-                if (dataArray[i][j] == 0)
-                    return false;
+            if (dataArray[row][i] == value){
+                return false;
             }
         }
-        return true;
-    }
-    public int generatedValidValue(int currentValue, int row, int column){
-        while (currentValue != 6){
-            if (validValue(++currentValue, row, column))
-                return currentValue;
-        }
-        return -1;
-    }
-    public boolean validValue(int value, int row, int column){
-        for (int i = 0; i < dataArray.length; i++){
-            if (dataArray[row][i] == value || dataArray[i][column] == value){
+
+        for (int j = 0; j < arraySize; j++){
+            if (dataArray[j][column] == value){
                 return false;
             }
         }
